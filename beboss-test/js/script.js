7becc7b130856341af0d607841d3444b624d99bd
garold664 <garold664@gmail.com>
2020-06-01 17:39:13 +0300

    обновляет страницу bboss.io-test

--- beboss-test/js/script.js
+++ beboss-test/js/script.js
@@ -1,81 +1,32 @@
+'use strict';
+var navBtn = document.querySelector('.header__nav-btn');
+var nav = document.querySelectorAll('.nav');
+var body = document.body;
 
-var options = {
-  url: "resources/franchise.json",
+nav[0].style.transition = 'none';
+nav[1].style.transition = 'none';
+body.classList.remove('no-js');
+setTimeout(function () {
+  nav[0].style.transition = '';
+  nav[1].style.transition = '';
+}, 300);
 
-  getValue: "name"
+navBtn.addEventListener('click', function (e) {
+  var curTarget = e.currentTarget;
 
-  // template: {
-  //     type: "links",
-  //     fields: {
-  //         link: "website-link"
-  //     }
-  // },
+  if (body.classList.contains('menu-closed')) {
 
-  // theme: "plate-dark"
-};
+    body.style.top = (parseInt(window.scrollY, 10) * -1) + 'px';
+    curTarget.classList.add('header__nav-btn--active');
+    body.classList.remove('menu-closed');
 
-$("#franchise").easyAutocomplete(options);
+    // body.style.position = 'fixed';
+  } else {
+    var scrollY = body.style.top;
+    curTarget.classList.remove('header__nav-btn--active');
+    body.classList.add('menu-closed');
 
-$(".easy-autocomplete")
-/* List show animation */
-.on("show.eac", function() {
-  console.log("Show EAC!!!");
-  this.classList.add("easy-autocomplete--opened");
-})
-.on("hide.eac", function() {
-  console.log("Hide EAC!!!");
-  this.classList.remove("easy-autocomplete--opened");
+    body.style.top = '';
+    window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+  }
 });
-
-var form = document.querySelector(".form__form");
-var submitBtn = document.querySelector(".form__submit");
-var telInput = document.getElementById("tel");
-var nameInput = document.getElementById("name");
-var franchiseInput = document.getElementById("franchise");
-var inputsValidity = [{telIsValid: false}, {nameIsValid: false}, {franchiseIsValid: false}];
-
-// form.addEventListener("invalid", function(evt) {
-//   console.log("evt.currentTarget = " + evt.currentTarget);
-//   console.log("evt.target = " + evt.target);
-
-//   if (evt.target.getAttribute("id") === "tel") {
-
-//     evt.preventDefault();
-//     console.log("telephone!");
-//     console.log(form.elements[1].validity);
-
-//     if (evt.target.validity.patternMismatch) {
-//       evt.target.parentNode.classList.add("pattern-mismatch");
-//     }
-//   }
-// }, true);
-
-function checkValidity(evt) {
-  var validity = evt.target.validity;
-  var parent = evt.target.parentNode;
-
-  if (!(validity.valid)) {
-    window.isValid = false;
-    parent.classList.add("invalid");
-
-    parent.classList.toggle("invalid--pattern", (validity.patternMismatch));
-    parent.classList.toggle("invalid--missing", (validity.valueMissing));
-
-  } else {
-    window.isValid = true;
-    parent.classList.remove("invalid", "invalid--pattern", "invalid--mismatch");
-  }
-
-  if ((telInput.validity.valid) && (nameInput.validity.valid) && (franchiseInput.validity.valid)) {
-    submitBtn.classList.remove("form__submit--disabled");
-    submitBtn.removeAttribute("disabled");
-  } else {
-    submitBtn.classList.add("form__submit--disabled");
-    submitBtn.setAttribute("disabled", "true");
-  }
-  
-}
-
-telInput.addEventListener("blur", checkValidity);
-nameInput.addEventListener("blur", checkValidity);
-franchiseInput.addEventListener("blur", checkValidity);
